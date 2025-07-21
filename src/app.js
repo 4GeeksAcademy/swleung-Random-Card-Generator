8966586b8dc4feb3e5da796d27ea9c8ba41de8b2
Samantha Leung <leung.sam62@gmail.com>
2025-07-21 20:30:11 +0000

    Samantha Leung - Random Card Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  const suits = [
+    { symbol: "♥", class: "heart" },
+    { symbol: "♦", class: "diamond" },
+    { symbol: "♠", class: "spade" },
+    { symbol: "♣", class: "club" }
+  ];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const card = document.getElementById("card");
+  card.classList.add(randomSuit.class);
+
+  document.getElementById("card-value").innerText = randomValue;
+
+  document.getElementById("top-suit").innerHTML = randomSuit.symbol;
+  document.getElementById("bottom-suit").innerHTML = randomSuit.symbol;
 };
